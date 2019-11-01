e2a6ea17bf3041a6b7cc688d757270862d305e17
VojtechFrommel <u.vojtech.frommel@gmail.com>
2019-11-01 10:33:12 +0100

    initial commit - Ukol

---
+++ Ukol/Ukol/ViewModels/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ukol.ViewModels
+{
+    public class Data
+    {
+        public int FromEntry { get; set; }
+        public int ToEntry { get; set; }
+        public int Range => ToEntry - FromEntry;
+
+        //generování random čísla
+
+    }
+}
