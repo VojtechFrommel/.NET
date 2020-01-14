6042b064fac3ec011c13614d28bc8524ed60a028
VojtechFrommel <u.vojtech.frommel@gmail.com>
2020-01-14 09:42:30 +0100

    interface, model change

--- Hangman/Hangman/Models/Data.cs
+++ Hangman/Hangman/Models/Data.cs
@@ -5,11 +5,20 @@ using System.Threading.Tasks;
 
 namespace Hangman.Models
 {
-    public static class Data
+    public class Data : IData
     {
-        public static string WordToGuess { get; set; }
-        public static string Winner { get; set; }
-        public static int NumOfPlayers { get; set; }
-        public static List<char> TriedChars = new List<char>();
+        private string _wordToGuess { get; set; }
+        private string _winner { get; set; }
+        private int _numOfPlayers { get; set; }
+        private List<char> _triedChars { get; set; }
+        public Data()
+        {
+
+        }
+
+        public string WordToGuess { get { return _wordToGuess; } set { _wordToGuess = value; } }
+        public string Winner { get { return _winner; } set { _winner = value; } }
+        public int NumOfPlayers { get { return _numOfPlayers; } set { _numOfPlayers = value; } }
+        public List<char> TriedChars { get { return _triedChars; } set { _triedChars = value; } }
     }
 }
---
+++ Hangman/Hangman/Models/IData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangman.Models
+{
+    public interface IData
+    {
+        string WordToGuess { get; set; }
+        string Winner { get; set; }
+        int NumOfPlayers { get; set; }
+        List<char> TriedChars { get; set; }
+    }
+}
